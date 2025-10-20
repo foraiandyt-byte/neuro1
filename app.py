f1c928184e3b715d50e86181e98323483221cb25
foraiandyt-byte <foraiandyt@gmail.com>
2025-10-20 11:01:42 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google as genai
 
 # Configure Gemini
-genai.configure(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")
+genai(api_key="AIzaSyALEjQpQpIEtZcEHCYrGOizaVITtD0Atxw")
 
 # Create model
 model = genai.GenerativeModel("gemini-2.5-flash")
@@ -37,4 +37,5 @@ if user_msg:
     # Display bot message
     st.chat_message("assistant").write(bot_reply)
     st.session_state.messages.append({"role": "assistant", "content": bot_reply})
+
 
